b9e940272483c5017b05b87801d876013730528e
rmar72 <rsolm72@gmail.com>
2019-07-29 18:06:10 -0600

    search bar handler

---
+++ pict-depict/src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component{
+
+    state = {
+        searchTerm: ''
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+        this.props.onSubmit(this.state.searchTerm)
+    }
+
+    render(){
+        return(
+            <div className="ui segment">
+                <form 
+                    className="ui form"
+                    onSubmit={this.onFormSubmit}
+                >
+                    <label>Image Search</label>
+                    <input 
+                        type="text" 
+                        className="field"
+                        value={ this.state.searchTerm }
+                        onChange={(e) => 
+                            this.setState({ searchTerm: e.target.value })}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
