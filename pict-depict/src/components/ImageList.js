16777bda6d1cf381fb192bc21718c46af108c40f
rmar72 <rsolm72@gmail.com>
2019-07-30 00:11:51 -0600

    images grid layout

--- pict-depict/src/components/ImageList.js
+++ pict-depict/src/components/ImageList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ImageList.css';
 
 const ImageList = (props) => {
 
@@ -6,7 +7,7 @@ const ImageList = (props) => {
         return <img key={id} src={urls.regular} alt={description} />
     })
 
-    return <div>{ images }</div>
+    return <div className="image-list">{ images }</div>
 }
 
 export default ImageList;
