5d190bc705df0e05037202882ec5a790e384af9f
rmar72 <rsolm72@gmail.com>
2019-07-29 18:04:58 -0600

    App initial workings

---
+++ pict-depict/src/components/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import unsplash from '../api/unsplash-config';
+import SearchBar from './SearchBar';
+
+class App extends Component {
+
+    state = {
+        images: []
+    };
+
+    // syntatic sugar for binding cb, use throughout app safely relying on 'this'
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term }
+        });
+
+        this.setState({ images: response.data.results });
+    }
+
+    render(){
+        // this.onSearchSubmit("cheetas"); // interesting find
+        // so from inspecting the this, I was saying that the this within the method
+        // is indeed pointing to this App class, bc its a class(object) method;
+        // in reality its a key holding a anon fn,
+        // and bc we actually pass it down as a prop to our child component
+        // whenever it gets invoked, it'll point to whats to the left of it
+        // so on the other side you'll see this.props.onSubmit()
+        // whats to the left of the invoked fn? Yup props! 
+        // Gotta use one of the 3 strategies: 
+        // constructor with bind, object method inline arrow fn, or wrap in arrow fn in an event cb 
+   
+        return(
+            <div className="ui container" style={{ marginTop: '15px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: { this.state.images.length } IMages
+            </div>
+        )
+    }
+
+}
+
+export default App;
